e61f4f4c5ad5f8a56a25d5c5d804430c1235461e
Lucasgarcc <luuccasgarciia@gmail.com>
2025-01-18 19:24:45 -0300

    refactor: :recycle: Implementando rotas com react-router-dom (outlet) para renderização de páginas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import UseFetch from './components/Hooks/UseFetch/UseFetch';
 
-// Pages Routes
-import Menu from './components/Menu/Menu';
-import Banner from './components/Banner/Index';
-import Register from './components/Pages/Form/Form';
-import Footer from './components/Footer/Footer';
-import Category from './components/Category/Category';
-import Emphasis from './components/Emphasis/Emphasis';
-import Detail from './components/Emphasis/Detail/Detail';
-import Search from './components/Pages/Search/Search'; 
 
-function App({}) {
+import AppRoutes from './components/Routes/AppRoutes';
+
+function App({ }) {
   const [categories, setCategories] = React.useState([
     {
       id: uuidv4(),
@@ -111,86 +103,17 @@ function App({}) {
   };  
   
   return (
-    <div className="App">
-      <Menu />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Banner />
-              <Detail />
-              {loading && <p style={{ color: '#000' }}>Carregando...</p>}
-              {error && <span style={{ color: 'red' }}>{error}</span>}
-              {!loading && data && ( <Emphasis
-                img={data.length > 0 ? data[data.length - 1].image : null}
-                title={data.length > 0 ? data[data.length -1].name : 'Nenhum livro disponível'}
-                description={
-                  data.length > 0
-                    ? data[data.length - 1].description
-                    : 'Adicione um livro para vê-lo em destaque.'
-                }
-                categories={categories.map((item) => item.name)}
-                registerBook={addNewBook}
-                registerCategory={addNewCategory}
-              />
-              )}
-
-              <h2 className="title">Recomenda Livros</h2>
-              {loading && <p style={{ color: '#000' }}>Carregando...</p>}
-              {error && <span style={{ color: 'red' }}>{error}</span>}
-              {!loading && data && (
-                categories.map((category) => (
-                  <Category
-                    key={category.id}
-                    category={category}
-                    title={category.name}
-                    book={data.filter((b) => b.category === category.name)}
-                    deleteCategory={deleteCategory}
-                    changeColorCategory={handleColorCategory}
-                    toggleFavorite={toggleFavorite}
-                  />
-                ))
-              )}
-            </>
-          }
-        />
-        <Route
-          path="/cadastro"
-          element={
-            <Register
-              categories={categories.map((item) => item.name)}
-              registerBook={addNewBook}
-              registerCategory={addNewCategory}
-            />
-          }
-        />
-        <Route
-          path="/buscar"
-          element={
-            <>
-              <Search />
-              {loading && <p style={{ color: '#000' }}>Carregando...</p>}
-              {error && <span style={{ color: 'red' }}>{error}</span>}
-              {!loading && data && (
-                categories.map((category) => (
-                  <Category
-                    key={category.id}
-                    category={category}
-                    title={category.name}
-                    book={data.filter((b) => b.category === category.name)}
-                    deleteCategory={deleteCategory}
-                    changeColorCategory={handleColorCategory}
-                    toggleFavorite={toggleFavorite}
-                  />
-                ))
-              )}
-            </>
-          }
-        />
-      </Routes>
-      <Footer />
-    </div>
+      <AppRoutes 
+        categories={categories}
+        data={data}
+        loading={loading}
+        error={error}
+        handleColorCategory={handleColorCategory}
+        addNewBook={addNewBook}
+        deleteCategory={deleteCategory}
+        addNewCategory={addNewCategory}
+        toggleFavorite={toggleFavorite}
+      />
   );
 }
 
