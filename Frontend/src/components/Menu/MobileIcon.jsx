636b7f9b9b205c08c9f7d4a94e0eb9bb5614d9cc
Lucasgarcc <luuccasgarciia@gmail.com>
2025-01-18 21:32:30 -0300

    feat: :sparkles: Implementação inicial do menu mobile, ajustes na estilização dos componentes

---
+++ Frontend/src/components/Menu/MobileIcon.jsx
@@ -0,0 +1,17 @@
+
+import './Menu.css';
+
+const MobileIcon = () => {
+
+  return (
+    <>
+      <div className='nav-menu-icon'>
+        <span className="bar1"></span>
+        <span className="bar2"></span>
+        <span className="bar3"></span>
+      </div>
+    </>
+  );
+};
+
+export default MobileIcon;
